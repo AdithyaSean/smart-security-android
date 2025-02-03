8780d5572dbad3fb592dd4582c7084895c55c4d4
Adithya Ekanayaka <adithyasean@outlook.com>
2025-02-03 20:26:07 +0530

    add ACCESS_WIFI_STATE permission and configure WebViewClient for video streams

--- app/src/main/java/com/nextstep/smartsecurity/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nextstep/smartsecurity/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.nextstep.smartsecurity.databinding.FragmentHomeBinding
@@ -19,26 +20,41 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-        ): View {
-          _binding = FragmentHomeBinding.inflate(inflater, container, false)
-          val root: View = binding.root
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
-          viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        val webView1: WebView = binding.webView1
+        val webView2: WebView = binding.webView2
 
-          val webView1: WebView = binding.webView1
-          val webView2: WebView = binding.webView2
+        // Configure WebView settings
+        webView1.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            mediaPlaybackRequiresUserGesture = false
+        }
+        webView2.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            mediaPlaybackRequiresUserGesture = false
+        }
 
-          webView1.loadUrl("http://192.168.2.2:2003/video_feed/1")
-          webView2.loadUrl("http://192.168.2.2:2003/video_feed/2")
+        // Set WebViewClient to handle page loading
+        webView1.webViewClient = WebViewClient()
+        webView2.webViewClient = WebViewClient()
 
-          binding.audioButton1.setOnClickListener {
-              // Add logic to handle audio for the first stream
-          }
+        webView1.loadUrl("http://192.168.2.2:2003/video_feed/1")
+        webView2.loadUrl("http://192.168.2.2:2003/video_feed/2")
 
-          binding.audioButton2.setOnClickListener {
-              // Add logic to handle audio for the second stream
-          }
+        binding.audioButton1.setOnClickListener {
+            // Add logic to handle audio for the first stream
+        }
+
+        binding.audioButton2.setOnClickListener {
+            // Add logic to handle audio for the second stream
+        }
 
         return root
     }
